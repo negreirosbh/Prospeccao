083d53a1f97f3cbc85a883f087a481ec954e568d
Willames Negreiros <negreirosbh@gmail.com>
2018-09-23 01:16:18 -0300

    foi acrescentado o recyclerView

--- app/src/main/java/e/willamesnegreiros/prospeccao/MainActivity.java
+++ app/src/main/java/e/willamesnegreiros/prospeccao/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity
     public TextView subtituloIpEstimadaPonto;
 
 
-
+    //teste
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
